0469ffa4e1a5c2c57b7def86b74ecb8f65a60e57
abm90 <moon@Austins-MBP.fios-router.home>
2016-11-06 21:51:54 -0500

    no message

---
+++ edu-toolbar/spec/edu-toolbar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import EduToolbarView from '../lib/edu-toolbar-view';
+
+describe('EduToolbarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
